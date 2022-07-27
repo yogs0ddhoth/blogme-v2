0324f295befe9cf4100920cc96dab76e2ec10b9e
yogs0ddhoth <blin7.webdev@gmail.com>
2022-07-27 14:47:29 -0400

    indent removed

--- db/__init__.py
+++ db/__init__.py
@@ -7,9 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-engine = create_engine(
-  getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0
-)
+engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
