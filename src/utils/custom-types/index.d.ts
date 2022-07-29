dce5b2f4bf7780b87c051400db47dcef3c23f497
yogs0ddhoth <blin7.webdev@gmail.com>
2022-07-29 02:53:08 -0400

    model types declared

--- src/utils/custom-types/index.d.ts
+++ src/utils/custom-types/index.d.ts
@@ -3,4 +3,35 @@ declare module "custom-types" {
     key_1: string
     key_2: string
   }
+
+  interface User {
+    id: number
+    name: string
+    email: string
+    password: string
+  }
+  interface Post {
+    id:number
+    title: string
+    text: string
+    user?: {
+      id: number
+      name: string
+    }
+    created_at: string
+    updated_at: string
+    vote_count: number
+  }
+  interface Comment {
+    id: number
+    text: string
+    user_id: number
+    post_id: number
+  }
+  
+  interface Action {
+    type: string
+
+
+  }
 }
