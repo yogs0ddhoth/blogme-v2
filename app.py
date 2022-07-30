5de0ea9625566ddc7482f4bac704ae6457392b98
yogs0ddhoth <blin7.webdev@gmail.com>
2022-07-30 15:22:45 -0400

    static route updated to accept all front end paths

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from db import init_db
 
 from controllers import api, graphql, users, posts
 
-def create_app(test_config = None):
+def create_app(test_config=None):
   # set up app:
-  app = Flask(__name__, static_folder='build', static_url_path = '')
+  app = Flask(__name__, static_folder='build', static_url_path='')
   # CORS(app) # comment out on deployment
 
   app.url_map.strict_slashes = False
@@ -15,13 +15,14 @@ def create_app(test_config = None):
   )
   init_db(app)
 
-  @app.route('/')
-  def index():
-    return app.send_static_file('index.html')
-
   app.register_blueprint(api)
   app.register_blueprint(users)
   app.register_blueprint(posts)
   app.register_blueprint(graphql)
+
+  @app.route('/', defaults={'path': ''})
+  @app.route('/<path>')
+  def index(path):
+    return app.send_static_file('index.html')
   
   return app
