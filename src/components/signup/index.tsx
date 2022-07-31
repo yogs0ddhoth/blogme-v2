c850c2c6cc33df02d5bee94acfd71bf05a0e5a75
yogs0ddhoth <blin7.webdev@gmail.com>
2022-07-31 17:55:53 -0400

    elems => mui, react-hook-form, useSignup() connected

--- src/components/signup/index.tsx
+++ src/components/signup/index.tsx
@@ -1,34 +1,116 @@
+import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+import { useSignup } from "../../api/mutations";
+
 export default function SignupForm() {
+  const signup = useSignup()
+  const {
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors }
+  } = useForm(
+    { defaultValues: {name: "", email: "", password: ""} }
+  );
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => event.preventDefault();
 
   return (
-    <div className="row">
-      <div className="col-md-6">
-        <h2>Signup</h2>
-
-        <form className="form signup-form">
-          <div className="form-group">
-            <label>
-              name:
-              <input className="form-input" type="text" id="name-signup" />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>
-              email:
-              <input className="form-input" type="text" id="email-signup" />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>
-              password:
-              <input className="form-input" type="password" id="password-signup" />
-            </label>
-          </div>
-          <div className="form-group">
-            <button className="btn btn-primary" type="submit">signup</button>
-          </div>
-        </form>
-      </div>
+    <div className="col-md-6">
+      <h2>Signup</h2>
+      <form 
+        className=""
+        onSubmit={ handleSubmit((data) => signup.mutate(data)) }
+      >
+        <FormControl className="name" 
+          required 
+          error={errors.name?.message !== undefined}
+        >
+          <Controller
+            name="name"
+            control={control}
+            render={ ({field}) => {
+              return (
+                <TextField {...field} 
+                  label="Name:" color="primary"
+                  helperText={errors.name?.message}
+                />
+              )
+            }}
+            rules={
+              {required: "This field is required."}
+            }
+          />
+        </FormControl>
+          
+        <FormControl className="email"  
+          required error={errors.email?.message !== undefined}
+        >
+          <Controller
+            name="email"
+            control={control}
+            render={ ({field}) => {
+              return (
+                <TextField {...field} 
+                  label="Email:" color="primary" helperText={errors.email?.message}
+                />
+              )
+            }}
+            rules={{
+              pattern: {
+                value: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+                message: "Enter a valid email address."
+              }
+            }}
+          />
+        </FormControl>
+          
+        <FormControl className="password">
+          <Controller
+            name="password"
+            control={control}
+            render={ ({field}) => (
+              <>
+                <InputLabel htmlFor="password">Password:</InputLabel>
+                <OutlinedInput {...field}
+                  id="password2" color="primary" 
+                  label="Password:"
+                  type={showPassword ? 'text' : 'password'}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton 
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff/> : <Visibility/>}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </>
+            )}
+          />
+        </FormControl>
+          
+        <Button type="submit" variant="outlined">
+          Submit
+        </Button>     
+      </form>
     </div>
   )
 }
