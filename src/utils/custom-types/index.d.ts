3d1ebdf50a743ab7db67a1b821d25b321d9a5fc2
yogs0ddhoth <blin7.webdev@gmail.com>
2022-08-01 02:32:31 -0400

    types updated to accomodate different data shapes

--- src/utils/custom-types/index.d.ts
+++ src/utils/custom-types/index.d.ts
@@ -2,26 +2,28 @@ declare module "custom-types" {
   interface User {
     id?: number
     name?: string
-    email: string
-    password: string
+    email?: string
+    password?: string
+    posts?: Post[]
   }
   interface Post {
     id?:number
     title?: string
     text?: string
-    user?: {
-      id: number
-      name: string
-    }
+    user?: User
     created_at?: string
     updated_at?: string
+    comments?:Comment[]
     vote_count?: number
   }
   interface Comment {
     id?: number
     text: string
     user_id?: number
-    post_id: number
+    post_id?: number
+    user?: User
+    created_at?: String
+    updated_at?: String
   }
   interface Vote {
     id?: number
