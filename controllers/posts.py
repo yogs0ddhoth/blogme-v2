8264c3de211b2307326fc92ee0d67758395ae00f
yogs0ddhoth <blin7.webdev@gmail.com>
2022-08-04 22:32:12 -0400

    most '/posts' routes require jwt, create() uses jwt

--- controllers/posts.py
+++ controllers/posts.py
@@ -1,6 +1,7 @@
 import sys
 import json
 from flask import Blueprint, request, jsonify, session
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from models import Post, Comment, Vote
 from db import get_db
 from utils.auth import login_required
@@ -9,7 +10,6 @@ from utils.filters import format_fields
 bp = Blueprint('posts', __name__, url_prefix='/posts')
 
 @bp.route('/', methods=['GET'])
-# @login_required
 def get_all_posts():
   print(session.get('user_id'))
   db = get_db() # connect to database
@@ -24,15 +24,17 @@ def get_all_posts():
   return json.dumps(posts)
 
 @bp.route('/', methods=['POST'])
-# @login_required
+@jwt_required()
 def create():
   data = request.get_json()
+  identity = get_jwt_identity()
   db = get_db()
   try:
+    print(identity)
     newPost = Post( # create new Post
       title = data['title'],
       text = data['text'],
-      user_id = session.get('user_id')
+      user_id = identity['id']
     )
     db.add(newPost)
     db.commit()
@@ -42,10 +44,9 @@ def create():
     db.rollback()
     return jsonify(message = 'Post failed'), 500
 
-  return jsonify(id = newPost.id)
+  return jsonify(id = newPost.id), 201
 
 @bp.route('/<id>', methods=['GET'])
-# @login_required
 def get(id):
   try:
     db = get_db()
@@ -63,7 +64,7 @@ def get(id):
     return jsonify(message = 'Posts not found'), 404
 
 @bp.route('/<id>', methods=['PUT'])
-# @login_required # require the user to be logged in
+@jwt_required()
 def update(id):
   data = request.get_json()
   db = get_db()
@@ -88,7 +89,7 @@ def update(id):
   return '', 204
 
 @bp.route('/<id>', methods=['DELETE'])
-# @login_required
+@jwt_required()
 def delete(id):
   db = get_db()
   try:
@@ -107,7 +108,7 @@ def delete(id):
   return '', 204
 
 @bp.route('/upvote', methods=['PUT'])
-# @login_required
+@jwt_required()
 def upvote():
   data = request.get_json()
   db = get_db()
@@ -127,9 +128,10 @@ def upvote():
   return '', 204
 
 @bp.route('/comment', methods=['Post'])
-# @login_required
+@jwt_required()
 def comment():
   data = request.get_json()
+  identity = get_jwt_identity()
   db = get_db()
   try:
     newComment = Comment( # create new Comment
