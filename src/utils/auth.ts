248f3d1770f8d1e1bfe42768c64ad87854f459b7
yogs0ddhoth <blin7.webdev@gmail.com>
2022-08-04 22:38:18 -0400

    getUser returns obj with user id, name, and auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,44 +1,57 @@
 import * as React from 'react';
 import decode, { JwtPayload } from "jwt-decode";
 
-export default function useAuth() {
+type Token = {
+  user:string 
+  sub: {
+    user:string
+    id:number 
+    email:string
+  }
+}
+export default function Auth() {
   function getToken() {
     const token = localStorage.getItem('token');
     return (token !== null) ? token : null;
   };
   function getUser() {
     const token = getToken()
-    return (token !== null) ? decode(token) : null
+    if (token === null) {
+      return {user: '', id: 0, auth: null}
+    }
+    const decodedToken = decode<Token>(token)
+    return {user:decodedToken.sub.user, id: decodedToken.sub.id, auth:token}
   };
-  const [token, setToken] = React.useState(getToken());
 
-  // function isTokenExpired(token:string) {
-  //   try {
-  //     const decodedToken:JwtPayload = decode(token);
-  //     // check if expiration time has passed
-  //    if (decodedToken.exp < Date.now()/1000) {
-  //      return true;
-  //    } else {
-  //      return false;
-  //    }
-  //   } catch (err) {
-  //     return false;
-  //   }
-  // }
+  function isTokenExpired(token:string) {
+    try {
+      const decodedToken= decode<JwtPayload>(token);
+      if (decodedToken.exp !== undefined) {
+        // check if expiration time has passed
+        if (decodedToken.exp < Date.now()/1000) {
+          return true;
+      }
+      } else {
+        return false;
+      }
+    } catch (err) {
+      return false;
+    }
+  };
+
   function saveUser(token:string) {
     localStorage.setItem('token', token);
-    setToken(token);
   };
 
   function clearUser() {
     localStorage.removeItem('token');
-    setToken(null);
   };
 
   return {
     saveUser,
-    userAuth: token,
-    getUser,
-    clearUser
+    isTokenExpired,
+    getToken,
+    clearUser,
+    getUser
   }
 }
