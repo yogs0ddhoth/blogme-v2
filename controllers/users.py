27eea99ca976876c97bebf69dfc5c17e2c9bfa94
yogs0ddhoth <blin7.webdev@gmail.com>
2022-08-04 22:33:03 -0400

    jwt now used to query db, token updated

--- controllers/users.py
+++ controllers/users.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime, timedelta, timezone
 from unicodedata import name
-from flask_jwt_extended import create_access_token, get_jwt, get_jwt_identity, jwt_required, unset_jwt_cookies
+from flask_jwt_extended import create_access_token, get_jwt, get_jwt_header, get_jwt_identity, jwt_required, unset_jwt_cookies
 import sqlalchemy
 from flask import Blueprint, request, jsonify, session
 from models import User, Post
@@ -30,13 +30,12 @@ def refresh_expiring_jwt(response):
 @bp.route('/', methods=['GET'])
 @jwt_required()
 def get_posts():
-  data = request.json.get('name')
-  identity = get_jwt()
+  identity = get_jwt_identity()
   print("!!!!!!!!!!!!!!!!!!!!!!!!!!", identity, "!!!!!!!!!!!!!!!!!!!!!!!!!!")
   db = get_db() # connect to database
   user = ( # identity['sub']: {'name': 'seed1', 'email': 'seed1@cbc.ca'}
     db.query(User)
-    .filter(User.email == identity['sub']['email'])
+    .filter(User.email == identity['email'])
     .one()
   ).as_dict()
   print(user)
@@ -66,11 +65,11 @@ def signup(): # req: {name:string, email:string, password:string}
     db.rollback()
     return jsonify(message='Signup Failed'), 500
 
-  access_token = create_access_token(identity={'name': data['name'], 'email': data['email']})
+  access_token = create_access_token(identity={'id': newUser.id, 'user': newUser.name, 'email': newUser.email})
   session.clear()
   session['user_id'] = newUser.id
   session['loggedIn'] = True
-  return jsonify(id=newUser.id, name=newUser.name, access_token=access_token)
+  return jsonify(id=newUser.id, user=newUser.name, access_token=access_token)
 
 @bp.route('/login', methods=['POST'])
 def login(): # req: {email:string, password:string}
@@ -89,11 +88,11 @@ def login(): # req: {email:string, password:string}
   if user.verify_password(data['password']) == False:
     return jsonify(message = 'Incorrect credentials'), 400
   
-  access_token = create_access_token(identity={'name': user.name, 'email': user.email})
+  access_token = create_access_token(identity={'id': user.id, 'user': user.name, 'email': user.email})
   session.clear()
   session['user_id'] = user.id
   session['loggedIn'] = True
-  return jsonify(id=user.id, name=user.name, access_token=access_token)
+  return jsonify(id=user.id, user=user.name, access_token=access_token)
 
 @bp.route('/logout', methods=['POST'])
 def logout(): # remove session variables
