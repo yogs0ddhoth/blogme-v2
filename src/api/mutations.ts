be71aabba66ca584f9237a3694fccda71f5e6a2f
yogs0ddhoth <blin7.webdev@gmail.com>
2022-08-04 22:40:36 -0400

    login, signup, logout now update state and localstorage

--- src/api/mutations.ts
+++ src/api/mutations.ts
@@ -1,43 +1,61 @@
 import { useMutation } from "@tanstack/react-query";
-import { Comment, Post, User } from "custom-types";
+import { AxiosResponse } from "axios";
+import { Comment, Post, User, AuthAction, Vote } from "custom-types";
+import React from "react";
 import { apiPosts, apiUsers } from ".";
+import { LOGIN, LOGOUT } from '../utils/context/actions';
 
-export function useSignup() {
+export function useSignup(dispatch:React.Dispatch<AuthAction>) {
   return useMutation( (data:User) => apiUsers('post', 'signup', undefined, data), {
-    // onSuccess: () => window.location.assign('/dashboard')
+    onSuccess: ({data}) => {
+      dispatch({
+        type:LOGIN, 
+        payload:{user: data.user, id: data.id, auth: data.access_token}
+      });
+      // window.location.assign('/dashboard');
+    }
   });
-};
-export function useLogin() {
+}
+export function useLogin(dispatch:React.Dispatch<AuthAction>) {
   return useMutation( (data:User) => apiUsers('post', 'login', undefined, data), {
-    // onSuccess: () => window.location.assign('/dashboard')
+    onSuccess: ({data}) => {
+      dispatch({
+        type:LOGIN, 
+        payload:{user: data.user, id: data.id, auth: data.access_token}
+      });
+      window.location.assign('/dashboard');
+    }
   });
-};
-export function useLogout() {
+}
+export function useLogout(dispatch:React.Dispatch<AuthAction>) {
   return useMutation( () => apiUsers('post', 'logout'), {
-    // onSuccess: () => window.location.assign('/')
-  })
-};
+    onSuccess: () => {
+      dispatch({type: LOGOUT});
+      window.location.assign('/');
+    }
+  });
+}
 
-export function useCreatePost(auth:string) {
+export function useCreatePost(auth:string|null) {
   return useMutation( (data:Post) => apiPosts('post', undefined, auth, data), {
+    onSuccess: () => window.location.reload()
+  });
+}
+export function useUpdatePost(auth:string|null, id:number) {
+  return useMutation( (data:Post) => apiPosts('put', id, auth, data), {
     // onSuccess: () => window.location.reload()
   });
-};
-export function useUpdatePost(auth:string) {
-  return useMutation( (data:{id:number, data:Post}) => apiPosts('put', data.id, auth, data.data), {
-    // onSuccess: () => window.location.reload()
+}
+export function useDeletePost(auth:string|null, id:number) {
+  return useMutation( () => apiPosts('delete', id, auth), {
+    onSuccess: () => window.location.reload()
   });
-};
-export function useDeletePost(auth:string) {
-  return useMutation( (id:number) => apiPosts('delete', id, auth), {
-    // onSuccess: () => window.location.reload()
-  });
-};
+}
 
-export function useComment(auth:string) {
+export function useComment(auth:string|null) {
   return useMutation( (data:Comment) => apiPosts('post', 'comment', auth, data) );
-};
+}
 
-export function useVote(auth:string) {
-  return useMutation( (post_id:number) => apiPosts('put', 'upvote', auth, {post_id}) )
+export function useVote(auth:string|null) {
+  return useMutation( (data:Vote) => apiPosts('put', 'upvote', auth, data) );
 }
