e8511817b8649e60f7d90329d095981a96ea8aba
yogs0ddhoth <blin7.webdev@gmail.com>
2022-08-09 23:50:03 -0400

    timestamp component extracted and modularized

---
+++ src/components/Timestamp/index.tsx
@@ -0,0 +1,15 @@
+import Typography from '@mui/material/Typography';
+
+interface TimestampProps {
+  created_at: string;
+  updated_at: string;
+}
+const Timestamp = ({created_at, updated_at}:TimestampProps) => (
+  <Typography>
+    {Date.parse(updated_at) > Date.parse(created_at) 
+      ? 'edited ' + new Date(updated_at).toLocaleDateString('en-us')
+      : new Date(created_at).toLocaleDateString('en-us')
+    }
+  </Typography>
+);
+export default Timestamp;
