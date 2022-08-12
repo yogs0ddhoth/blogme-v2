4fba8827e355c5611446b9cb7c8af798cdf76004
yogs0ddhoth <blin7.webdev@gmail.com>
2022-08-12 12:21:08 -0400

    modal styling moved to taliwind

--- src/components/StyledModal/index.tsx
+++ src/components/StyledModal/index.tsx
@@ -11,17 +11,9 @@ interface PopupProps {
 export default function Popup(props: PopupProps) {
   return (
     <Modal {...props}>
-      <Box
+      <Box className='styled-modal'
         sx={{
-          position: 'absolute' as 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          width: 500,
-          bgcolor: 'background.paper',
-          // border: '2px solid #000',
-          boxShadow: 24,
-          // p: 4,
+          bgcolor: 'background.paper'
         }}
       >
         {props.children}
