c4eb34a7fc4f936fe93f229bd074bbd30cb3e4a0
yogs0ddhoth <blin7.webdev@gmail.com>
2022-08-15 22:42:51 -0400

    voteButton extracted from vote, conditional color

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -1,10 +1,13 @@
-import * as React from 'react'
+import * as React from 'react';
 
-import IconButton from '@mui/material/IconButton'
+import IconButton from '@mui/material/IconButton';
 
-import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined'
-import LogoutIcon from '@mui/icons-material/Logout'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
+import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LogoutIcon from '@mui/icons-material/Logout';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+import theme from '../../utils/mui-theme';
 
 export const CloseButton = (props: { onClick: () => void }) => (
   <IconButton onClick={props.onClick}>
@@ -13,8 +16,8 @@ export const CloseButton = (props: { onClick: () => void }) => (
 )
 
 interface LogoutButtonProps {
-  className: string
-  onClick: () => void
+  className: string;
+  onClick: () => void;
 }
 export const LogoutButton = (props: LogoutButtonProps) => (
   <IconButton onClick={props.onClick}>
@@ -23,11 +26,21 @@ export const LogoutButton = (props: LogoutButtonProps) => (
 )
 
 interface MenuButtonProps {
-  className: string
-  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+  className: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 export const MenuButton = (props: MenuButtonProps) => (
   <IconButton id="button" className={props.className} onClick={props.onClick}>
     <MoreVertIcon />
+  </IconButton>
+)
+
+interface VoteButtonProps {
+  onClick: () => void;
+  upVoted: boolean;
+}
+export const VoteButton = ({onClick, upVoted}: VoteButtonProps) => (
+  <IconButton onClick={onClick}>
+    <FavoriteIcon sx={upVoted ? {color: theme.palette.error.light} : {}} />
   </IconButton>
 )
