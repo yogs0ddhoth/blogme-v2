53fe98ff1ed1bfa113d12ac351b208f878719c11
yogs0ddhoth <blin7.webdev@gmail.com>
2022-08-19 22:25:51 -0400

    api updated for controllers

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,28 +1,26 @@
 import axios from 'axios';
-import { Signup, Login, CommentInput, PostInput, Vote } from 'custom-types';
 
-export default function api<PathType, DataType>(input: string) {
-  return async <ResponseData>(
-    method: string,
-    path?: PathType,
-    data?: DataType,
-    auth?: string | null
-  ) => {
-    try {
-      const url = input;
-      return await axios.request<ResponseData>({
-        method: method,
-        url: url + `${path ? path : ''}`,
-        headers:
-          typeof auth === 'string'
-            ? {
-                Authorization: 'Bearer ' + auth,
-              }
-            : {},
-        data: data ? data : {},
-      });
-    } catch (error) {
-      throw new Error(`${error}`);
-    }
-  };
+export default async function api<PathType, DataType, ResponseData>(
+  method: string,
+  url: string,
+  path?: PathType,
+  data?: DataType,
+  auth?: string
+) {
+  try {
+    return await axios.request<ResponseData>({
+      method: method,
+      url: url + `${path ? path : ''}`,
+      headers:
+        auth
+          ? {
+              Authorization: 'Bearer ' + auth,
+            }
+          : {},
+      data: data ? data : {},
+    });
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
 }
+
